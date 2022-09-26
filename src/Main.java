4a8a8cbdddffd7fa616cc21d89cda8829871e244
ArtSitof <art-sidof@yandex.ru>
2022-09-26 16:20:10 +0500

    Added search for the city with the largest population

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -6,17 +7,28 @@ public class Main {
 
         // Этап 1
         // Загрузка данных справочника
-        UtilsForDirectory.createAListOfCities().forEach(System.out::println);
+//        UtilsForDirectory.createAListOfCities().forEach(System.out::println);
 
         // Этап 2
         // Сортировки
-        List<City> directoryOfCities = UtilsForDirectory.createAListOfCities();
+//        List<City> directoryOfCities = UtilsForDirectory.createAListOfCities();
         //Сортировка по имени города
-        sortByNameOfCity(directoryOfCities);
-        printDirectoryOfCities(directoryOfCities);
+//        sortByNameOfCity(directoryOfCities);
+//        printDirectoryOfCities(directoryOfCities);
         // Сортировка по федеральному округу и имени города
-        sortByDistrictAndNameOfCity(directoryOfCities);
-        printDirectoryOfCities(directoryOfCities);
+//        sortByDistrictAndNameOfCity(directoryOfCities);
+//        printDirectoryOfCities(directoryOfCities);
+
+        // Этап 3
+        // Поиск города с наибольшим количеством жителей
+        List<City> directoryOfCities = UtilsForDirectory.createAListOfCities();
+        // Вариант 1
+//        System.out.println(directoryOfCities.stream().max(Comparator.comparing(City::getPopulation)));
+        // Вариант 2
+        City maxPopulation = maxPopulation(directoryOfCities.toArray(new City[directoryOfCities.size()]));
+        System.out.println("Наибольшее количество жителей в городе " + maxPopulation.getNameOfCity()
+                    + " составляет - " + maxPopulation.getPopulation() + " человек.");
+
     }
 
     // Сортировка списка городов по наименованию в алфавитном порядке по убыванию без учета регистра
@@ -35,5 +47,14 @@ public class Main {
         for(City c : listCities) {
             System.out.println(c);
         }
+    }
+
+    // Поиск города с наибольшим количеством жителей
+    public static City maxPopulation(City[] array) {
+        if(array.length == 2) {
+            return array[0].getPopulation() > array[1].getPopulation() ? array[0] : array[1];
+        }
+        City subMax = maxPopulation(Arrays.copyOfRange(array, 1, array.length));
+            return array[0].getPopulation() > subMax.getPopulation() ? array[0] : subMax;
     }
 }
